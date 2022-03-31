f69f5581be30701c79e6673e5bf2354186177ada
ManojMichaelX0 <98898332+ManojMichaelX0@users.noreply.github.com>
2022-03-31 22:02:46 +0530

    Update UC-3-Insert_data_to_Table.sql

--- UC-3-Insert_data_to_Table.sql
+++ UC-3-Insert_data_to_Table.sql
@@ -5,6 +5,4 @@ Insert into employee_payroll (Name,Salary,StartDate) values('Manoj',80000,GETDAT
 Insert into employee_payroll (Name,Salary,StartDate) values('Laxmi',13000,GETDATE())
 Insert into employee_payroll (Name,Salary,StartDate) values('Manoj',5000000,GETDATE())
 
-select *
-from employee_payroll
-Go
+
