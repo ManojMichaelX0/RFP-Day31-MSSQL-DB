11c3441ddba32989f86d756464945b559c19e0e8
ManojMichaelX0 <98898332+ManojMichaelX0@users.noreply.github.com>
2022-04-01 18:35:28 +0530

    Update UC-9-Extend-Pay.sql

--- UC-9-Extend-Pay.sql
+++ UC-9-Extend-Pay.sql
@@ -1,23 +1,22 @@
 use payroll_service
 Go
 
-Insert into employee_payroll (Name,Salary,StartDate) values('Manoj',80000,GETDATE())
-Insert into employee_payroll (Name,Salary,StartDate) values('Laxmi',13000,GETDATE())
-Insert into employee_payroll (Name,Salary,StartDate) values('Manoj',5000000,GETDATE())
-
-Select Salary 
-From employee_payroll
-Where Name='Manoj'
-Go
-
-Select *
-From employee_payroll
-Where StartDate Between Cast('2018-01-01' As Date) And GETDATE() 
-
-
 Alter Table employee_payroll
-Add BasicPay money,Deductions money,TaxablePay money,IncomeTax money,NetPay money
+Add BasicPay money
+Go
+Alter Table employee_payroll
+Add Deduction money
+Go
+Alter Table employee_payroll
+Add  TaxablePay money
+Go
+Alter Table employee_payroll
+Add IncomeTax money
+Go
+Alter Table employee_payroll
+Add NetPay money
 Go
 
 Select *
 From employee_payroll
+
